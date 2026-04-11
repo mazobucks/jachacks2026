f47f63158c9cac22ade46d981239545bfc272d64
Ifthi <ifthi.uddin@dawsoncollege.qc.ca>
2026-04-11 16:19:56 -0400

    Added a db and login feature

--- app.py
+++ app.py
@@ -1,29 +1,56 @@
 import os
 import sqlite3
 import secrets
-from flask import Flask, render_template, g
+import werkzeug
+from flask import Flask, render_template, request, redirect, url_for, flash, g
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+def hash_password(name, password):
+    return werkzeug.security.generate_password_hash(name + password)
 
 path = "study.db"
 database_exists = os.path.isfile(path)
 db = sqlite3.connect(path)
 if not database_exists:
     db.execute(
-        "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), grade VARCHAR(255))"
+        "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) UNIQUE, grade VARCHAR(255), password VARCHAR(255))"
     )
     db.execute(
         "CREATE TABLE Stats (id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255), points INTEGER NOT NULL CHECK(points >= 0 AND points <= 100), user_id INTEGER NOT NULL, FOREIGN KEY (user_id) REFERENCES Users(id))"
     )
+    db.execute(
+        "INSERT INTO Users (name, grade, password) VALUES (?, ?, ?)",
+        ("Test", "A", hash_password("Test", "1234")),
+    )
     db.commit()
+    db.close()
+
+class User(UserMixin):
+    def __init__(self, id, name, password):
+        self.id = name
+        self.user_id = id
+        self.name = name
+        self.password = password
 
 def get_db():
     db = g.get("_database")
     if not db:
-        db = sqlite3.connect(path)
-        g._database = db
+        g._database = sqlite3.connect(path)
+        db = g._database
     return db
+
+@login_manager.user_loader
+def user_loader(name):
+    record = get_db().execute("SELECT id, password, name FROM Users WHERE name = ? LIMIT 1", [name]).fetchone()
+    if not record:
+        return None
+    return User(record[0], record[2], record[1])
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -34,6 +61,28 @@ def close_connection(exception):
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/login")
+def login_form():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form["name"]
+    password = request.form["password"]
+    record = get_db().execute("SELECT id, password FROM Users WHERE name = ? LIMIT 1", [name]).fetchone()
+    if not record or not werkzeug.security.check_password_hash(record[1], name + password):
+        flash("Login info invalid!!!")
+        return redirect(url_for("login_form"))
+    user = User(record[0], name, record[1])
+    login_user(user)
+    return redirect(url_for("home"))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
