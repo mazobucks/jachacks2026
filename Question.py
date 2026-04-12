b8b7de6a3743de9778d45d7b87eee637c605fee6
Ifthi <ifthi.uddin@dawsoncollege.qc.ca>
2026-04-12 04:19:49 -0400

    quiz object

--- Question.py
+++ Question.py
@@ -2,3 +2,6 @@ class Question:
     def __init__(self, question, answers):
         self.question = question
         self.answers = answers
+        
+    def __str__(self):
+        return f"question: {self.question}, answers: {self.answers}"
